431f2cfe4b5e2a1ef87713c62467c0db0bee3cdc
adesinaroland <adesinaroland@gmail.com>
2022-07-26 09:34:14 -0400

    Updates

--- src/main/java/com/codedifferently/Solution.java
+++ src/main/java/com/codedifferently/Solution.java
@@ -1,7 +1,24 @@
 package com.codedifferently;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Solution {
     public String howManyLettersDoYouSee(String input){
+        Map<Character, Integer> wordCount = new TreeMap<>();
+
+        char[] charArr = input.toCharArray();
+
+        for (Character character : charArr){
+            if(!wordCount.containsKey(character))
+                wordCount.put(character, 1);
+            else{
+                int count = wordCount.get(character);
+                wordCount.put(character, count + 1);
+            }
+        }
+
+
         return null;
     }
 }
